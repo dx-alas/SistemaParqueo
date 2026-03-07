9da34f7abeec07b1da5817a5f7f6a8d6017ec802
Daniel Alas <daniel.alas2007@gmail.com>
2026-03-07 17:41:00 -0600

    Agregar archivos de proyecto.

---
+++ SistemaParqueo.Entities/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaParqueo.Entities
+{
+    public class Class1
+    {
+    }
+}
