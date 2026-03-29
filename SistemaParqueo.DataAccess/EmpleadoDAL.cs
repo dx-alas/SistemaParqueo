190c9f7be4403c40a5e22a05090513977059724a
Daniel Alas <daniel.alas2007@gmail.com>
2026-03-28 18:15:15 -0600

    Capa EmpleadoDAL agregada

---
+++ SistemaParqueo.DataAccess/EmpleadoDAL.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SistemaParqueo.Entities;
+using System.Data;
+
+namespace SistemaParqueo.DataAccess
+{
+    public class EmpleadoDAL : Connection
+    {
+        private static EmpleadoDAL _instance;
+        public static EmpleadoDAL Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new EmpleadoDAL();
+                return _instance;
+            }
+        }
+
+        public bool Insert(Empleado entity)
+        {
+            bool result = false;
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("spInsertEmpleado", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@Nombre", entity.Nombre);
+                    cmd.Parameters.AddWithValue("@Apellido", entity.Apellido);
+                    cmd.Parameters.AddWithValue("@DUI", entity.DUI);
+                    cmd.Parameters.AddWithValue("@Correo", (object)entity.Correo ?? DBNull.Value);
+                    cmd.Parameters.AddWithValue("@Telefono", (object)entity.Telefono ?? DBNull.Value);
+                    cmd.Parameters.AddWithValue("@EstadoEmpleadoId", entity.EstadoEmpleadoId);
+                    conn.Open();
+                    result = cmd.ExecuteNonQuery() > 0;
+                }
+            }
+            return result;
+        }
+
+        public bool Update(Empleado entity)
+        {
+            bool result = false;
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("spUpdateEmpleado", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@EmpleadoId", entity.EmpleadoId);
+                    cmd.Parameters.AddWithValue("@Nombre", entity.Nombre);
+                    cmd.Parameters.AddWithValue("@Apellido", entity.Apellido);
+                    cmd.Parameters.AddWithValue("@DUI", entity.DUI);
+                    cmd.Parameters.AddWithValue("@Correo", (object)entity.Correo ?? DBNull.Value);
+                    cmd.Parameters.AddWithValue("@Telefono", (object)entity.Telefono ?? DBNull.Value);
+                    cmd.Parameters.AddWithValue("@EstadoEmpleadoId", entity.EstadoEmpleadoId);
+                    conn.Open();
+                    result = cmd.ExecuteNonQuery() > 0;
+                }
+            }
+            return result;
+        }
+
+        public bool Delete(int empleadoId)
+        {
+            bool result = false;
+            using (SqlConnection conn = new SqlConnection(_connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand("spDeleteEmpleado", conn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@EmpleadoId", empleadoId);
+                    conn.Open();
+                    result = cmd.ExecuteNonQuery() > 0;
+                }
+            }
+            return result;
+        }
+    }
+}
