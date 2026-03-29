24bfd864ada1b691380e3ee471eed2dc38ef7c00
Daniel Alas <daniel.alas2007@gmail.com>
2026-03-28 18:19:47 -0600

    Nueva entidad EstadoEmpleado agregada

---
+++ SistemaParqueo.Entities/EstadoEmpleado.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaParqueo.Entities
+{
+    public class EstadoEmpleado
+    {
+        public int EstadoEmpleadoId { get; set; }
+        public string Nombre { get; set; }
+
+        public EstadoEmpleado()
+        {
+
+        }
+
+        public EstadoEmpleado(string nombre)
+        {
+            Nombre = nombre;
+        }
+
+        public EstadoEmpleado(int estadoEmpleadoId, string nombre)
+        {
+            EstadoEmpleadoId = estadoEmpleadoId;
+            Nombre = nombre;
+        }
+    }
+}
