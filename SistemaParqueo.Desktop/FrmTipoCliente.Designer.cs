0eb39604d2de0276d57ea459da182fda15fd0088
Daniel Alas <daniel.alas2007@gmail.com>
2026-04-27 19:56:56 -0600

    Formulario TipoCliente agregado

---
+++ SistemaParqueo.Desktop/FrmTipoCliente.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace SistemaParqueo.Desktop
+{
+    partial class FrmTipoCliente
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.dgvTipoCliente = new System.Windows.Forms.DataGridView();
+            this.lblListaEstado = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TipoClienteId = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Nombre = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTipoCliente)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 22F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(298, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(487, 50);
+            this.label1.TabIndex = 46;
+            this.label1.Text = "GESTIÓN DE TIPO CLIENTE";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(81, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 28);
+            this.label2.TabIndex = 67;
+            this.label2.Text = "Id:";
+            // 
+            // txtId
+            // 
+            this.txtId.Enabled = false;
+            this.txtId.Font = new System.Drawing.Font("Segoe UI", 9.75F);
+            this.txtId.Location = new System.Drawing.Point(274, 151);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(210, 29);
+            this.txtId.TabIndex = 66;
+            // 
+            // dgvTipoCliente
+            // 
+            this.dgvTipoCliente.AllowUserToAddRows = false;
+            this.dgvTipoCliente.AllowUserToDeleteRows = false;
+            this.dgvTipoCliente.AllowUserToOrderColumns = true;
+            this.dgvTipoCliente.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvTipoCliente.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.dgvTipoCliente.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvTipoCliente.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTipoCliente.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.TipoClienteId,
+            this.Nombre});
+            this.dgvTipoCliente.Location = new System.Drawing.Point(86, 431);
+            this.dgvTipoCliente.Name = "dgvTipoCliente";
+            this.dgvTipoCliente.ReadOnly = true;
+            this.dgvTipoCliente.RowHeadersWidth = 51;
+            this.dgvTipoCliente.RowTemplate.Height = 24;
+            this.dgvTipoCliente.Size = new System.Drawing.Size(884, 247);
+            this.dgvTipoCliente.TabIndex = 65;
+            // 
+            // lblListaEstado
+            // 
+            this.lblListaEstado.AutoSize = true;
+            this.lblListaEstado.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.lblListaEstado.Location = new System.Drawing.Point(80, 380);
+            this.lblListaEstado.Name = "lblListaEstado";
+            this.lblListaEstado.Size = new System.Drawing.Size(210, 32);
+            this.lblListaEstado.TabIndex = 64;
+            this.lblListaEstado.Text = "LISTADO ESTADO";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(77, 93);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(189, 32);
+            this.label4.TabIndex = 63;
+            this.label4.Text = "DATOS ESTADO";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.Red;
+            this.btnEliminar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnEliminar.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnEliminar.ForeColor = System.Drawing.Color.White;
+            this.btnEliminar.Location = new System.Drawing.Point(357, 284);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(112, 39);
+            this.btnEliminar.TabIndex = 62;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.BackColor = System.Drawing.Color.DodgerBlue;
+            this.btnActualizar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnActualizar.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnActualizar.ForeColor = System.Drawing.Color.White;
+            this.btnActualizar.Location = new System.Drawing.Point(213, 284);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(125, 39);
+            this.btnActualizar.TabIndex = 61;
+            this.btnActualizar.Text = "Actualizar";
+            this.btnActualizar.UseVisualStyleBackColor = false;
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackColor = System.Drawing.Color.Gold;
+            this.btnLimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLimpiar.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnLimpiar.ForeColor = System.Drawing.Color.White;
+            this.btnLimpiar.Location = new System.Drawing.Point(493, 284);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(112, 39);
+            this.btnLimpiar.TabIndex = 60;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = false;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuardar.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            this.btnGuardar.ForeColor = System.Drawing.Color.White;
+            this.btnGuardar.Location = new System.Drawing.Point(83, 284);
+            this.btnGuardar.Margin = new System.Windows.Forms.Padding(0);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(112, 39);
+            this.btnGuardar.TabIndex = 59;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Segoe UI", 9.75F);
+            this.txtNombre.Location = new System.Drawing.Point(274, 218);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(210, 29);
+            this.txtNombre.TabIndex = 58;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(81, 219);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 28);
+            this.label3.TabIndex = 57;
+            this.label3.Text = "Nombre:";
+            // 
+            // TipoClienteId
+            // 
+            this.TipoClienteId.DataPropertyName = "TipoClienteId";
+            this.TipoClienteId.HeaderText = "Id Tipo Cliente";
+            this.TipoClienteId.MinimumWidth = 6;
+            this.TipoClienteId.Name = "TipoClienteId";
+            this.TipoClienteId.ReadOnly = true;
+            // 
+            // Nombre
+            // 
+            this.Nombre.DataPropertyName = "Nombre";
+            this.Nombre.HeaderText = "Nombre de tipo cliente";
+            this.Nombre.MinimumWidth = 6;
+            this.Nombre.Name = "Nombre";
+            this.Nombre.ReadOnly = true;
+            // 
+            // FrmTipoCliente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1047, 770);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.dgvTipoCliente);
+            this.Controls.Add(this.lblListaEstado);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Name = "FrmTipoCliente";
+            this.Text = "Gestión de Tipo Cliente";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTipoCliente)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.DataGridView dgvTipoCliente;
+        private System.Windows.Forms.Label lblListaEstado;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnActualizar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn TipoClienteId;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Nombre;
+    }
+}
