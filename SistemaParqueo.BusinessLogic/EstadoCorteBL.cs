adb7e146f622b8a2ea113f1aca10257801a6b253
Katherine <katy.canizalez10@gmail.com>
2026-05-08 16:48:31 -0600

    Formulario EstadoCorte con funciones completas agregado

---
+++ SistemaParqueo.BusinessLogic/EstadoCorteBL.cs
@@ -0,0 +1,101 @@
+﻿using SistemaParqueo.DataAccess;
+using SistemaParqueo.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaParqueo.BusinessLogic
+{
+    public class EstadoCorteBL
+    {
+        private static EstadoCorteBL _instance;
+        public static EstadoCorteBL Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new EstadoCorteBL();
+                return _instance;
+            }
+        }
+
+        public bool Insert(EstadoCorte entity)
+        {
+            bool result = false;
+
+            try
+            {
+                result = EstadoCorteDAL.Instance.Insert(entity);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return result;
+        }
+
+        public bool Update(EstadoCorte entity)
+        {
+            bool result = false;
+
+            try
+            {
+                result = EstadoCorteDAL.Instance.Update(entity);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return result;
+        }
+
+        public bool Delete(int estadoCorteId)
+        {
+            bool result = false;
+
+            try
+            {
+                result = EstadoCorteDAL.Instance.Delete(estadoCorteId);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return result;
+        }
+
+        public EstadoCorte SelectById(int estadoCorteId)
+        {
+            EstadoCorte result = null;
+
+            try
+            {
+                result = EstadoCorteDAL.Instance.SelectById(estadoCorteId);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return result;
+        }
+
+        public List<EstadoCorte> SelectAll()
+        {
+            List<EstadoCorte> result = null;
+
+            try
+            {
+                result = EstadoCorteDAL.Instance.SelectAll();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ SistemaParqueo.Entities/EstadoCorte.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaParqueo.Entities
+{
+    public class EstadoCorte
+    {
+        public int EstadoCorteId { get; set; }
+        public string Nombre { get; set; }
+
+        public EstadoCorte()
+        {
+
+        }
+
+        public EstadoCorte(string nombre)
+        {
+            Nombre = nombre;
+        }
+
+        public EstadoCorte(int estadoCorteId, string nombre)
+        {
+            EstadoCorteId = estadoCorteId;
+            Nombre = nombre;
+        }
+    }
+}
